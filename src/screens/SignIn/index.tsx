e2c68e1ca06113555fda0fe7c4b635be2e89897c
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2022-01-13 11:49:11 -0300

    Componente de Input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import Container from "./styles";
+import Input from '../../components/Input';
+import Container from './styles';
 
 export default function SignIn() {
-  return <Container>{/* Todo */}</Container>;
+  return (
+    <Container>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+    </Container>
+  );
 }
