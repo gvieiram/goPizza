c8ef250160411be3fafbe8f08d266669af94aa25
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2022-01-14 02:05:08 -0300

    Input Picker

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import * as ImagePicker from 'expo-image-picker';
 
 import { ButtonBack } from '@components/ButtonBack';
 import { Photo } from '@components/Photo';
@@ -15,6 +17,23 @@ import {
 } from './styles';
 
 export function Product() {
+  const [image, setImage] = useState('');
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      });
+
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -26,9 +45,13 @@ export function Product() {
         </TouchableOpacity>
       </Header>
       <Upload>
-        <Photo uri="" />
+        <Photo uri={image} />
 
-        <PickImageButton title="Carregar" type="secondary" />
+        <PickImageButton
+          title="Carregar"
+          type="secondary"
+          onPress={handlePickerImage}
+        />
       </Upload>
     </Container>
   );
