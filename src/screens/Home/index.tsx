a26716151cbdadf61b0951aa83a9dfdcd3bc2c4f
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2022-01-17 12:52:25 -0300

    Listando e realizando consulta no banco

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert, FlatList } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import firestore from '@react-native-firebase/firestore';
 import { useTheme } from 'styled-components/native';
 
 import { MaterialIcons } from '@expo/vector-icons';
 
 import happyEmoji from '@assets/happy.png';
-import { ProductCard } from '@components/ProductCard';
+import { ProductCard, ProductProps } from '@components/ProductCard';
 import { Search } from '@components/Search';
 
 import {
@@ -22,7 +24,46 @@ import {
 } from './styles';
 
 export function Home() {
+  const [pizzas, setPizzas] = useState<ProductProps[]>([]);
+  const [search, setSearch] = useState('');
+
   const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection('pizzas')
+      .orderBy('name_insensitive')
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then(response => {
+        const data = response.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+        setPizzas(data);
+      })
+      .catch(() =>
+        Alert.alert('Consulta', 'Não foi possível realizar a consulta '),
+      );
+  }
+
+  function handleSearch() {
+    fetchPizzas(search);
+  }
+
+  function handleSearchClear() {
+    setSearch('');
+    fetchPizzas('');
+  }
+
+  useEffect(() => {
+    fetchPizzas('');
+  }, []);
 
   return (
     <Container>
@@ -37,19 +78,29 @@ export function Home() {
         </TouchableOpacity>
       </Header>
 
-      <Search onSearch={() => {}} onClear={() => {}} />
+      <Search
+        onChangeText={setSearch}
+        value={search}
+        onSearch={handleSearch}
+        onClear={handleSearchClear}
+      />
 
       <MenuHeader>
         <Title>Cardápio</Title>
-        <MenuItemsNumber>10 pizzas</MenuItemsNumber>
+        <MenuItemsNumber>
+          {pizzas.length} {pizzas.length === 1 ? 'pizza' : 'pizzas'}
+        </MenuItemsNumber>
       </MenuHeader>
 
-      <ProductCard
-        data={{
-          id: '1',
-          name: 'Adicionando UI',
-          description: 'Teste UI',
-          photo_url: 'https://github.com/gvieiram.png',
+      <FlatList
+        data={pizzas}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <ProductCard data={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingTop: 20,
+          paddingBottom: 125,
+          marginHorizontal: 24,
         }}
       />
     </Container>
